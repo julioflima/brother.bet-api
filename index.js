c3b8ffe842ed929a6022eaf2e625cd60c6536e6c
juloko <julio_flima@hotmail.com>
2019-03-12 20:58:43 -0300

    add libraries

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const port = 9001;
 
 //Routers requires and calls.
 app.use('/birds', require('./src/birds'))
-// app.use('/readable', require('./src/readable'))
-// app.use('/readable', require('./src/readable'))
-// app.use('/readable', require('./src/readable'))
-// app.use('/readable', require('./src/readable'))
-// app.use('/readable', require('./src/readable'))
 
 app.get("/", (req, res) => {
     res.send("Hello te fode!")
